706220912a30d65cde3218e88c1a8cd6c395208b
alertguo <1196614612@qq.com>
2020-08-01 21:07:04 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-let html = document.querySelector('#demo') //通过CSS选择器找到这个元素
+let html = document.querySelector('#html') //通过CSS选择器找到这个元素
 let style = document.querySelector('#style')
 
 let string = `
@@ -8,8 +8,8 @@ let string = `
 **/
 #div1{
   border: 1px solid red;
-  width: 400px;
-  height: 400px;
+  width: 200px;
+  height: 200px;
 }
 /*接下来我把 div 变成一个太极
 *注意看好了
@@ -28,8 +28,8 @@ let string = `
 }
 /*加两个风火轮*/
 #div1::before{
-  width: 200px;
-  height: 200px;
+  width: 100px;
+  height: 100px;
   top: 0;
   left: 50%;
   transform:  translateX(-50%);
@@ -38,8 +38,8 @@ let string = `
   background: radial-gradient(circle, rgba(0,0,0,1) 0%, rgba(0,0,0,1) 25%, rgba(255,255,255,1) 25%, rgba(255,255,255,1) 100%);
 }
 #div1::after{
-  width: 200px;
-  height: 200px;
+  width: 100px;
+  height: 100px;
   bottom: 0;
   left: 50%;
   transform: translateX(-50%);
@@ -67,8 +67,10 @@ let step = () => {
     }
     // string2 += string[n] === '\n' ? '<br>' : string[n]
     html.innerHTML = string2
-    style.innerHTML = string.substring(0, n) 
+    style.innerHTML = string.substring(0, n)
     //CSS就不能直接获取string2了，否则会出现 HTML 代码
+    window.scrollTo(0, 99999)//竖向滚动条
+    html.scrollTo(0, 99999)
     if (n < string.length - 1) {
       // 如果 n 不是最后一个，就继续
       n += 1
